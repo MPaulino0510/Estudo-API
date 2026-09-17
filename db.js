e83dda6ece2907decd2d5e327bd25115b594e1bd
Miguel Paulino Alves Pereira <miguelpaulino2009.ap@gmail.com>
2026-09-17 20:22:15 -0300

    Salvando alterações

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
-
 const conexao = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -8,5 +7,6 @@ const conexao = mysql.createPool({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
-
 module.exports = conexao;
+
+// O .env, é uma forma de deixar o código mais seguro, afinal, ele cria uma variável com sua senha, nome da sua database, usuário, sua host e porta, e "esconde ela" de possíveis bots no próprio github, que poderiam eventualmente roubar sua senha. Ela é extremamente importante, provavelmente a informação mais útil relacionada a repositórios e banco de dados.
--- db.sql
+++ db.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE if NOT EXISTS cadastros;
+CREATE DATABASE if NOT EXISTS cadastros; 
+-- Cria uma database nova (se caso já não existir uma database com esse nome).
 
 USE cadastros;
+-- Usa a database nova criada.
 
 CREATE TABLE if NOT EXISTS alunos (
+-- Cria uma tabela nova (se caso não exista numa tabela com esse nome).
+
 id INT AUTO_INCREMENT PRIMARY KEY,
+-- Adiciona uma variável (tipo INT) para chamar os Ids, o "AUTO_INCREMENT" serve para já adicionar um Id novo para cada usuário novo que estiver sendo criado, facilitando a busca pelo Id. A chave primária, basicamente ajuda a identificar de forma única cada linha de tabela, para auxiliar a ligar esse (no caso o Id) a outras chaves estrangeiras.
+
 nome VARCHAR(100) NOT NULL,
+-- Cria uma variável tipo "String" (no caso VARCHAR) com no máximo 100 caractéres, que não pode ficar vazia.
+
 curso VARCHAR(100) NOT NULL
 );
 
@@ -12,6 +20,10 @@ INSERT INTO alunos (nome, curso) VALUES
 ('Miguel', 'Música'),
 ('Cris', 'Coral'),
 ('Belinha', 'Pet');
+-- Já adiciona de forma "Manual" algumas informações para a tabela.
 
 SHOW TABLES;
+-- Mostra todas as tabelas.
+
 SELECT * FROM alunos;
+-- Mostra todas informações das tabelas.
